4f7948392e8e98f382c767d504e98a11a32f2721
Dave Naugler <davenaugler@gmail.com>
2024-03-27 20:31:21 -0600

    Evening commit - half way into Section 10

--- Sec.10-Revisiting_Data_Types.sql
+++ Sec.10-Revisiting_Data_Types.sql
@@ -57,3 +57,89 @@ SELECT * FROM parent ORDER BY children DESC;
 -- Hello Dave this is a git test
 -- Laptop says, it worked
 
+DROP TABLE parent;
+
+SELECT * FROM parent;
+
+
+CREATE TABLE parent (children TINYINT UNSIGNED);
+
+INSERT INTO parent (children) VALUES (3);
+
+SELECT * FROM parent;
+
+INSERT INTO parent (children) VALUE (255);
+
+SELECT * FROM parent;
+
+DESC parent;
+
+INSERT INTO parent (children) VALUE (1.5);
+
+SELECT * FROM parent;
+
+INSERT INTO parent (children) VALUE (90.4);
+
+SELECT * FROM parent;
+
+-- DECIMAL
+
+CREATE TABLE products (price DECIMAL(5,2));
+
+INSERT INTO products (price) VALUE (50.05);
+
+SELECT * FROM products;
+
+INSERT INTO products (price) VALUE (4.50);
+
+SELECT * FROM products;
+
+-- This is a test comment to practice switching from DataGrip to Chrome using keyboard shortcut.
+
+INSERT INTO products (price) VALUE (456.99);
+SELECT * FROM products;
+
+INSERT INTO products(price) VALUE (5.026);
+SELECT * FROM products;
+
+INSERT INTO products (price) VALUE (5.029);
+SELECT * FROM products;
+
+-- FLOAT & DOUBLE
+--    FLOAT  ->  4 bytes of memory ->  Precision Issues happen at ~7
+--    DOUBLE ->  8 bytes of memory ->  Precision Issues happen at ~15
+
+CREATE TABLE numbers (x FLOAT, y DOUBLE);
+
+INSERT INTO numbers (x,y) VALUES (1.123, 1.123);
+SELECT * FROM numbers;
+
+INSERT INTO numbers (x,y) VALUES (2.12345, 2.12345);
+SELECT * FROM numbers;
+
+INSERT INTO numbers (x,y) VALUES (1.12345678, 1.12345678);
+SELECT * FROM numbers;
+
+
+INSERT INTO numbers (x,y) VALUES (3.12345678, 3.12345678901234567);
+SELECT * FROM numbers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
