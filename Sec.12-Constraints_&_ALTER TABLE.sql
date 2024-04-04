c63fc7016172a4653bd88e5b8ce727e6fc99c64f
Dave Naugler <davenaugler@gmail.com>
2024-04-04 08:53:49 -0600

    Multi-Column Checks

--- Sec.12-Constraints_&_ALTER TABLE.sql
+++ Sec.12-Constraints_&_ALTER TABLE.sql
@@ -94,4 +94,101 @@ SELECT * FROM palindromes;
 -- NAMED Constraints
 -- ------------------
 
+-- (From main Dev1 above to Dev2 below. See if this will run and work on Dev2 w/o creating it's own separate query files.)
 
+CREATE TABLE users2 (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    age INT,
+    CONSTRAINT age_18_or_over CHECK (age >= 18),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO users2 (username, age)
+    VALUE ('Chicken Lady', 18);
+
+INSERT INTO users2 (username, age)
+    VALUE ('Not Chicken Lady', 17);
+
+-- NAMED Constraints are important because when you have a CHECK that's being ran against your SQL code
+--   and a error comes back, you want a clearly understood name (variable name) for that CHECK.
+
+
+SELECT * FROM users2;
+
+DESC users2;
+
+CREATE TABLE palindrome2 (
+    id INT AUTO_INCREMENT,
+    word VARCHAR(100),
+    CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word),
+    PRIMARY KEY(id)
+);
+
+DESC palindrome2;
+
+INSERT INTO palindrome2 (word)
+    VALUE ('mom');
+
+SELECT * FROM palindrome2;
+
+-- Error: Check constraint 'word_is_palindrome' is violated.
+--   This is a good thing
+INSERT INTO palindrome2 (word)
+    VALUE ('momma');
+
+-- Multi-Column Checks
+-- --------------------
+
+-- The combination of name and address must be unique. Individually they can be the same.
+CREATE TABLE companies (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT name_address UNIQUE (name, address)
+);
+
+INSERT INTO companies (name, address)
+    VALUE ('Top Company Inc.', '135 Spruce Ln.');
+
+SELECT * FROM companies;
+
+INSERT INTO companies (name, address)
+    VALUE ('Mary\'s Pies', '135 Spruce Ln.');
+
+SELECT * FROM companies;
+
+-- Error: Duplicate entry 'Top Company Inc.-135 Spruce Ln.' for key 'companies.name_address'
+--   This is a good thing
+INSERT INTO companies (name, address)
+    VALUE ('Top Company Inc.', '135 Spruce Ln.');
+
+-- Pretend we flip houses
+CREATE TABLE houses (
+    id INT AUTO_INCREMENT,
+    purchase_price INT NOT NULL,
+    sale_price INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT salePrice_gt_purchasePrice CHECK(sale_price >= purchase_price)
+);
+
+DESC houses;
+
+INSERT INTO houses (purchase_price, sale_price)
+VALUE (100, 200);
+
+SELECT * FROM houses;
+
+-- Error: Check constraint 'salePrice_gt_purchasePrice' is violated.
+--   This is a good thing
+INSERT INTO houses (purchase_price, sale_price)
+VALUE (300, 250);
+
+INSERT INTO houses (purchase_price, sale_price)
+VALUE (300, 300);
+
+SELECT * FROM houses;
+
+-- Alter Table
+-- --------------------
