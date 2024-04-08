7a1d780a94281ec6e2713fc874a33cb505968e5f
Dave Naugler <davenaugler@gmail.com>
2024-04-08 10:18:20 -0600

    INNER JOIN and GROUP BY

--- Sec.13-One_to_Many_&_Joins.sql
+++ Sec.13-One_to_Many_&_Joins.sql
@@ -68,3 +68,71 @@ JOIN orders ON customers.id = orders.customer_id;
 -- This gives all the data from both tables
 SELECT * FROM orders
 JOIN customers ON customers.id = orders.customer_id;
+
+-- INNER JOIN and GROUP BY
+-- --------------------------
+DROP TABLE customers;
+DROP TABLE orders;
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO customers (first_name, last_name, email)
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+
+USE customers;
+-- WORKING
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+-- Review from previous lesson
+SELECT customers.first_name, customers.last_name, orders.order_date, orders.amount FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+-- Display the total amount each customer has spent
+--    GROUP BY first_name and last_name then display total amount spent
+SELECT customers.first_name, customers.last_name, SUM(orders.amount) AS total FROM customers
+JOIN orders ON orders.customer_id = customers.id
+GROUP BY first_name, last_name;
+-- first_name | last_name | total_spent
+-- -------------------------------------
+--    Boy     |  George   |  135.49
+--    George  |  Michael  |  813.17
+--    Bette   |  Davis    |  450.25
+
+-- This will allow us to order via total spent from greatest to least
+SELECT customers.first_name, customers.last_name, SUM(orders.amount) AS total FROM customers
+JOIN orders ON orders.customer_id = customers.id
+GROUP BY first_name, last_name
+ORDER BY total DESC;
+-- first_name | last_name | total_spent
+-- -------------------------------------
+--    George  |  Michael  |  813.17
+--    Bette   |  Davis    |  450.25
+--    Boy     |  George   |  135.49
+
+-- LEFT JOIN
+-- ------------
