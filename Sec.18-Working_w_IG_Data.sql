6c6170844a4a529ec75e68d08bfec507e12b5d9e
Dave Naugler <davenaugler@gmail.com>
2024-04-18 09:09:28 -0600

    Instagram Query Challenges

--- Sec.18-Working_w_IG_Data.sql
+++ Sec.18-Working_w_IG_Data.sql
@@ -1,2 +1,101 @@
 -- Section 18: Working with Lots of IG Data
 -- -----------------------------------------
+
+USE ig_clone;
+
+SELECT COUNT(*) FROM likes;
+
+SHOW TABLES;
+
+SELECT COUNT(*) AS comments_count FROM comments;        -- 7,488
+SELECT COUNT(*) AS follows_count FROM follows;          -- 7,623
+SELECT COUNT(*) AS likes_count FROM likes;              -- 8,782
+SELECT COUNT(*) AS photo_tags_count FROM photo_tags;    -- 501
+SELECT COUNT(*) AS photos_count FROM photos;            -- 257
+SELECT COUNT(*) AS tags_count FROM tags;                -- 21
+SELECT COUNT(*) AS users_count FROM users;              -- 100
+-- -----------------------------------------
+
+-- Instagram Challenge #1
+-- -----------------------------------------
+
+
+
+
+
+
+
+
+
+-- -----------------------------------------
+
+-- Instagram Challenge #2
+-- -----------------------------------------
+
+
+
+
+
+
+
+
+
+
+-- -----------------------------------------
+
+-- Instagram Challenge #3
+-- -----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+-- -----------------------------------------
+
+-- Instagram Challenge #4
+-- -----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+-- -----------------------------------------
+
+-- Instagram Challenge #5
+-- -----------------------------------------
+
+
+
+
+
+
+
+
+-- -----------------------------------------
+    
+-- Instagram Challenge #6
+-- -----------------------------------------
+
+
+
+
+
+
+
+-- -----------------------------------------
+
+-- Instagram Challenge #7
+-- -----------------------------------------
